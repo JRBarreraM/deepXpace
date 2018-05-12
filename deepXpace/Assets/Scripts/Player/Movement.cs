474dcc44f7a18860c2eea81b760229f846351274
Carlos Sivira <carlos.csivira@gmail.com>
2018-05-12 15:18:51 -0400

    Arreglando el entorno
    coloque carpetas para manejar mejor el proyecto

---
+++ deepXpace/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		float fixedSpeed = speed * Time.deltaTime;
+		rb.velocity = new Vector2 (Input.GetAxis("Horizontal") * fixedSpeed ,Input.GetAxis("Vertical") * fixedSpeed);
+	}
+}
