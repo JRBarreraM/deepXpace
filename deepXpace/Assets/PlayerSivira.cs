386bd442872058dfcaf99d5728cde98a4b82118f
José R. Barrera M <35788695+JRBarreraM@users.noreply.github.com>
2018-07-23 19:22:58 -0400

    Add files via upload

---
+++ deepXpace/Assets/PlayerSivira.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSivira : MonoBehaviour {
+
+	float speedX;
+	float speedY;
+	float left;
+	float right;
+	float up;
+	float down;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			left = -1;
+		} else {
+			left = 0;
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			right = 1;
+		} else {
+			right = 0;
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			down = -1;
+		} else {
+			down = 0;
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			up = 1;
+		} else {
+			up = 0;
+		}
+
+		speedX = (left + right) * 3f * Time.deltaTime;
+		speedY = (up + down) * 3f * Time.deltaTime;
+
+		transform.position = new Vector3 (speedX + transform.position.x, speedY + transform.position.y, transform.position.z);
+	}
+}
