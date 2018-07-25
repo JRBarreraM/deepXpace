b4282f78b85c22d3927dd7a8eacb89ebbc9b0e1a
Carlos Sivira <carlos.csivira@gmail.com>
2018-07-24 21:29:52 -0400

    Mejorando al enemigo simple

--- deepXpace/Assets/Scripts/SimpleEnemy/SimpleEnemyAtack.cs
+++ deepXpace/Assets/Scripts/SimpleEnemy/SimpleEnemyAtack.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SimpleEnemyAtack : MonoBehaviour {
 
@@ -9,18 +7,35 @@ public class SimpleEnemyAtack : MonoBehaviour {
 	public float shootRate; //Tiempo que pasa entre cada disparo de proyectil
 
 	private Vector3 initialPosition; //Posicion inicial del target
+	private Vector3 playerPosition; //Posicion actual del objeto player
+	private Vector3 direction; //Direccion de disparo
 
 	void Start () {
 		initialPosition = target.transform.position; //Guardamos la posicion inicial para usarla mas tarde
 	}
 
 	void Update () {
+		playerPosition = GameObject.Find ("Player").transform.position;
+		direction = target.transform.position - transform.position;
+
 		if (SimpleEnemyGroupMovement.state == SimpleEnemyGroupMovement.simpleEnemyStates.Moving) { //Verificamos si el enemigo se esta moviendo
-			Debug.Log ("Ataque Uno"); //Usamos el tipo de ataque en movimiento
+			MoovingAtack();
 		} 
 
 		if (SimpleEnemyGroupMovement.state == SimpleEnemyGroupMovement.simpleEnemyStates.Waiting) { //Verificamos si el enemigo esta esperando
-			Debug.Log ("Ataque Dos"); //Usamos el tipo de ataque en espera
+			WaitingAtack();
 		} 
+	}
+
+	void MoovingAtack() {
+		target.transform.position = transform.position + initialPosition;
+	}
+
+	void WaitingAtack() {
+		target.transform.position = playerPosition;
+	}
+
+	void Shoot() {
+		
 	}
 }
---
+++ deepXpace/Assets/Scripts/SimpleEnemy/SimpleEnemyBulletBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SimpleEnemyBulletBehavior : MonoBehaviour {
+
+	private Transform target;
+	private float speed;
+
+	void SetTarget(Vector3 _target) {
+		target.position = _target;
+	}
+
+	void SetSpeed(float _speed) {
+		speed = _speed;
+	}
+
+	void Start () {
+		transform.Translate (target.position * speed * Time.deltaTime,Space.World);
+	}
+}
