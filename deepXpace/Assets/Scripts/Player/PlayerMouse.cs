903c2a0356df72d1ca0ad5444f0e36dc42d291a1
José R. Barrera M <35788695+JRBarreraM@users.noreply.github.com>
2018-07-24 21:28:02 -0400

    Fixed

--- deepXpace/Assets/Scripts/Player/PlayerMouse.cs
+++ deepXpace/Assets/Scripts/Player/PlayerMouse.cs
@@ -3,36 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMouse : MonoBehaviour {
-
-	public float mLeftBorder;
-	public float mRightBorder;
-	public float mTopBorder;
-	public float mBottomBorder;
-
+	public float speed;				// Valores ajustados a la escena piloto actual
+	public float layer;				//0
+	public float mLeftBorder;		//-0.4f
+	public float mRightBorder;		//0.4f
+	public float mTopBorder;		//-0.9f
+	public float mBottomBorder;		//0.9f
 	private Transform mTransform;
 
 	void Awake () {
 		mTransform = transform;
 	}
 
-	void Start () {
-		mLeftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0.05f,0,0)).x;
-		mRightBorder = Camera.main.ViewportToWorldPoint(new Vector3(0.95f,0,0)).x;
-		mTopBorder = Camera.main.ViewportToWorldPoint(new Vector3(0,0.05f,0)).y;
-		mBottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0,0.095f,0)).y;
-
+	void Start () { //incializamos al jugador al fondo y en el centro
+		Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(0,mBottomBorder,Camera.main.nearClipPlane));
+		mTransform.position = pos;
 		Cursor.visible = false;
 	}
 	
-	void Update () {
+	void FixedUpdate() {
 		UpdateMovement();
 	}
 
 	void UpdateMovement() {
-		Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,Camera.main.nearClipPlane));
+		Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(speed+Input.mousePosition.x,speed+Input.mousePosition.y,Camera.main.nearClipPlane));
 		pos.x = Mathf.Clamp(pos.x,mLeftBorder,mRightBorder);
 		pos.y = Mathf.Clamp(pos.y,mTopBorder,mBottomBorder);
-		pos.z = 0;
+		pos.z = layer;
 		mTransform.position = pos;
 	}
 
